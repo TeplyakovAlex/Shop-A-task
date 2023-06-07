018bc6d9155b5bc5194b9c9dc10bccd3afaaab60
admin <teplyakov.in@gmail.com>
2023-06-07 19:32:42 +0200

    Fix bug

---
+++ src/app/models/OrderA.java
@@ -0,0 +1,12 @@
+package app.models;
+
+public class OrderA extends Order {
+
+    public OrderA(String code) {
+        super(code);
+    }
+
+    public double getOrderCost(int quota,double price ) {
+        return quota * price;
+    }
+}
---
+++ src/app/models/OrderB.java
@@ -0,0 +1,16 @@
+package app.models;
+
+public class OrderB extends Order {
+
+    double delivery;
+
+    public OrderB(String code, double delivery) {
+
+        super(code);
+        this.delivery = delivery;
+    }
+
+    public double getOrderCost(double quantity, double price) {
+        return  quantity * price + delivery;
+    }
+}
